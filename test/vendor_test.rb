9056dd33806085f7636d12c6924dfaac2f87b162
Chris Stankus <chrisstankus99@gmail.com>
2020-02-26 10:08:16 -0700

    Create item test, vendor test, and vendor class.

---
+++ test/vendor_test.rb
@@ -0,0 +1,69 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/item'
+require './lib/vendor'
+
+class VendorTest < Minitest::Test
+
+  def setup
+    @item1 = Item.new({name: 'Peach', price:
+    @item2 = Item.new({name: 'Tomato', price:
+  end
+
+  def test_it_exists
+    assert_instance_of
+  end
+
+  def test_attributes
+  end
+end
+
+# ```ruby
+# pry(main)> require './lib/item'
+# #=> true
+#
+# pry(main)> require './lib/vendor'
+# #=> true
+#
+# pry(main)> @item1 = Item.new({name: 'Peach', price: "$0.75"})
+# #=> #<Item:0x007f9c56740d48...>
+#
+# pry(main)> @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+# #=> #<Item:0x007f9c565c0ce8...>
+#
+# pry(main)> item2.name
+# #=> "Tomato"
+#
+# pry(main)> item2.price
+# #=> 0.50
+#
+# pry(main)> vendor = Vendor.new("Rocky Mountain Fresh")
+# #=> #<Vendor:0x00007f85683152f0...>
+#
+# pry(main)> vendor.name
+# #=> "Rocky Mountain Fresh"
+#
+# pry(main)> vendor.inventory
+# #=> {}
+#
+# pry(main)> vendor.check_stock(item1)
+# #=> 0
+#
+# pry(main)> vendor.stock(item1, 30)
+#
+# pry(main)> vendor.inventory
+# #=> {#<Item:0x007f9c56740d48...> => 30}
+#
+# pry(main)> vendor.check_stock(item1)
+# #=> 30
+#
+# pry(main)> vendor.stock(item1, 25)
+#
+# pry(main)> vendor.check_stock(item1)
+# #=> 55
+#
+# pry(main)> vendor.stock(item2, 12)
+#
+# pry(main)> vendor.inventory
+# #=> {#<Item:0x007f9c56740d48...> => 55, #<Item:0x007f9c565c0ce8...> => 12}
+# ```
